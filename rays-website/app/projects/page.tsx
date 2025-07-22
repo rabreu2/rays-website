b3b8e335e1616f869cb4aba7e01ec816ba10831c
rabreu2 <rayabreu2296@gmail.com>
2025-07-22 13:11:38 -0400

    /projects fully responsive on screen size

--- rays-website/app/projects/page.tsx
+++ rays-website/app/projects/page.tsx
@@ -7,23 +7,23 @@ import Image from "next/image";
 
 export default function Projects() {
   return (
-    <div className="flex items-center justify-center bg-black w-full">
-      <div className="w-full mx-10">
+    <div className="transition-all duration-300 ease-in-out flex items-center justify-center bg-black w-full">
+      <div className="w-full px-5 lg:px-10">
         <div className="flex items-center justify-center">
-          <h1 className="text-white text-5xl sm:text-7xl my-[100px]">
+          <h1 className="text-white text-5xl md:text-6xl xl:text-7xl mb-[65px] sm:mb-[120px] md:my-[80px] lg:my-[100px] 2xl:my-[150px]">
             My Projects
           </h1>
         </div>
-        <div className="flex w-full justify-start mb-[50px]">
+        <div className="flex w-full justify-start mb-[65px] sm:mb-[120px] md:mb-[75px] lg:mb-[100px]">
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 2.0, ease: "easeOut" }}
-            className="z-[100] flex items-center w-[65rem] h-[43rem] bg-[linear-gradient(_to_bottom_right,rgba(255,255,255,0.33),rgba(86,196,149,0.33)_)]">
+            className="z-[100] flex items-center min-w-[17rem] min-h-[13rem] sm:min-w-[25rem] sm:min-h-[17rem] md:min-w-[33rem] md:min-h-[22rem] lg:min-w-[48rem] lg:min-h-[30rem] xl:min-w-[55rem] xl:min-h-[36rem] 2xl:min-w-[65rem] 2xl:min-h-[43rem] bg-[linear-gradient(_to_bottom_right,rgba(255,255,255,0.33),rgba(86,196,149,0.33)_)]">
             <Image
               src={Recipe4MePreview}
               alt="Recipe4Me Page Preview"
-              className="z-[100] w-[41rem] sm:w-[55rem]"
+              className="z-[100] w-[13rem] sm:w-[20rem] md:w-[26rem] lg:w-[40rem] xl:w-[47rem] 2xl:w-[55rem]"
               priority
             />
           </motion.div>
@@ -31,41 +31,41 @@ export default function Projects() {
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 2.0, ease: "easeOut" }}
-            className="relative text-white flex flex-col items-start justify-center right-[5rem]">
-            <h2 className="text-5xl font-semibold mb-5">Recipe4Me</h2>
-            <p className="text-xl max-w-md mb-5">
+            className="relative text-white flex flex-col items-start justify-center right-[2.5rem] md:right-[5rem]">
+            <h2 className="text-xl sm:text-3xl md:text-5xl font-semibold mb-2 md:mb-5">Recipe4Me</h2>
+            <p className="text-xs sm:text-sm md:text-base xl:text-xl max-w-md mb-2 md:mb-5">
               Full stack recipe search engine built with React, TypeScript, Next.js API routes and MongoDB
             </p>
-            <a target="_blank" rel="noopener noreferrer" href='https://recipe4me.vercel.app/' className="text-2xl mb-5">
+            <a target="_blank" rel="noopener noreferrer" href='https://recipe4me.vercel.app/' className="text-base sm:text-xl xl:text-2xl mb-2 md:mb-5">
               <h3>
                 APPLICATION
               </h3>
             </a>
-            <a target="_blank" rel="noopener noreferrer" href='https://github.com/rabreu2/recipe4me' className="text-2xl mb-5">
-              <h3 className="text-2xl">
+            <a target="_blank" rel="noopener noreferrer" href='https://github.com/rabreu2/recipe4me' className="text-base sm:text-xl xl:text-2xl mb-2 md:mb-5">
+              <h3>
                 CODEBASE
               </h3>
             </a>
           </motion.div>
         </div>
 
-        <div className="flex w-full justify-end mb-[50px]">
+        <div className="flex w-full justify-end mb-[65px] sm:mb-[120px] md:mb-[75px] lg:mb-[100px]">
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 2.0, ease: "easeOut" }}
-            className="relative text-white text-right flex flex-col items-end justify-center left-[5rem]">
-            <h2 className="text-5xl font-semibold mb-5">JankJep (WIP)</h2>
-            <p className="text-xl max-w-md mb-5">
+            className="relative text-white text-right flex flex-col items-end justify-center left-[2.5rem] md:left-[5rem]">
+            <h2 className="text-xl sm:text-3xl md:text-5xl font-semibold mb-2 md:mb-5">JankJep (WIP)</h2>
+            <p className="text-xs sm:text-sm md:text-base xl:text-xl max-w-md mb-2 md:mb-5">
               Jeopardy! inspired game built with React, TypeScript, Next.js API routes and Cluebase API
             </p>
-            <a target="_blank" rel="noopener noreferrer" href='https://www.jankjep.com/' className="text-2xl mb-5">
+            <a target="_blank" rel="noopener noreferrer" href='https://www.jankjep.com/' className="text-base sm:text-xl xl:text-2xl mb-2 md:mb-5">
               <h3>
                 APPLICATION
               </h3>
             </a>
-            <a target="_blank" rel="noopener noreferrer" href='https://github.com/rabreu2/jank-jep' className="text-2xl mb-5">
-              <h3 className="text-2xl">
+            <a target="_blank" rel="noopener noreferrer" href='https://github.com/rabreu2/jank-jep' className="text-base sm:text-xl xl:text-2xl mb-2 md:mb-5">
+              <h3>
                 CODEBASE
               </h3>
             </a>
@@ -74,11 +74,11 @@ export default function Projects() {
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 2.0, ease: "easeOut" }}
-            className="z-[100] flex items-center justify-end w-[65rem] h-[43rem] bg-[linear-gradient(_to_bottom_right,rgba(255,255,255,0.33),rgba(86,196,149,0.33)_)]">
+            className="z-[100] flex items-center justify-end min-w-[17rem] min-h-[13rem] sm:min-w-[25rem] sm:min-h-[17rem] md:min-w-[33rem] md:min-h-[22rem] lg:min-w-[48rem] lg:min-h-[30rem] xl:min-w-[55rem] xl:min-h-[36rem] 2xl:min-w-[65rem] 2xl:min-h-[43rem] bg-[linear-gradient(_to_bottom_right,rgba(255,255,255,0.33),rgba(86,196,149,0.33)_)]">
             <Image
               src={JankJepPreview}
               alt="JankJep Page Preview"
-              className="w-[41rem] sm:w-[55rem]"
+              className="z-[100] w-[13rem] sm:w-[20rem] md:w-[26rem] lg:w-[40rem] xl:w-[47rem] 2xl:w-[55rem]"
               priority
             />
           </motion.div>
